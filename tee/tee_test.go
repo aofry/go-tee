3ac3fca93cfe32b2ba25636c76d6456ab77173d2
adi <adi.ofry@gmail.com>
2017-03-20 17:45:39 +0200

    check test header

--- tee/tee_test.go
+++ tee/tee_test.go
@@ -5,7 +5,9 @@ import "net/http"
 import (
 	"testing"
 	//"bytes"
+	log "github.com/Sirupsen/logrus"
 	"github.com/vulcand/oxy/testutils"
+	"io/ioutil"
 	"net/http/httptest"
 )
 
@@ -16,6 +18,7 @@ type TraceSuite struct{}
 //var _ = Suite(&TraceSuite{})
 
 func TestNextHandlerWorks(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
 	//create some handler
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Length", "5")
@@ -30,9 +33,19 @@ func TestNextHandlerWorks(t *testing.T) {
 	srv := httptest.NewServer(teeHandler)
 	defer srv.Close()
 
-	res, _, err := testutils.MakeRequest(srv.URL+"/hello", testutils.Method("POST"), testutils.Body("123456"))
+	res, _, err := testutils.MakeRequest(srv.URL+"/db1", testutils.Method("POST"), testutils.Body("123456"))
 
 	if res.StatusCode != http.StatusOK || err != nil {
 		t.Fatal("Error posting")
 	}
+
+	if res.Header.Get("Content-Length") != "5" {
+		t.Error("Content-Length did not pass")
+	}
+
+	//buf := &bytes.Buffer{}
+	//res.Body.Read(buf)
+	//res.Body.Read()
+	body, err := ioutil.ReadAll(res.Body)
+	log.Debug(body)
 }
