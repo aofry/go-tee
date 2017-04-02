2e7681664aab9a2a431fbd5599f35e8d60729365
adi <adi.ofry@gmail.com>
2017-04-02 11:28:05 +0300

    add test for util

--- util/getenv.go
+++ util/getenv.go
@@ -1,11 +1,22 @@
 package util
 
-import "os"
+import (
+	"github.com/Sirupsen/logrus"
+	"os"
+)
 
-func Getenv(key, fallback string) string {
+func Getenv(key string, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return fallback
+	}
+	return value
+}
+
+func GetenvNoDefault(key string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		logrus.Warn("Must provide env var: ", key)
 	}
 	return value
 }
---
+++ util/getnev_test.go
@@ -0,0 +1,43 @@
+package util
+
+import "testing"
+
+var SOME_DEFAULT = "kdfnsdlkfj23523sdgdfh"
+
+func TestGetenvExist(t *testing.T) {
+	res := Getenv("HOME", SOME_DEFAULT)
+
+	if res == "" {
+		t.Error("Existing env var exist, should not return empty string")
+	}
+	if res == SOME_DEFAULT {
+		t.Error("Existing env var exist, should not return the default")
+	}
+}
+
+func TestGetenvNotExistExpectDefault(t *testing.T) {
+	res := Getenv(SOME_DEFAULT, SOME_DEFAULT)
+
+	if res == "" {
+		t.Error("var does not exist, should return default")
+	}
+	if res != SOME_DEFAULT {
+		t.Error("var does not exist, should return default")
+	}
+}
+
+func TestGetenvNoDefault(t *testing.T) {
+	res := GetenvNoDefault("HOME")
+
+	if res == "" {
+		t.Error("Existing env var exist, should not return empty string")
+	}
+}
+
+//func TestGetenvNoDefaultWithNotExistingEnv(t *testing.T) {
+//	res := GetenvNoDefault(SOME_DEFAULT, SOME_DEFAULT)
+//
+//	if res != "" {
+//		t.Error("Env var does not exist, should return empty string")
+//	}
+//}
