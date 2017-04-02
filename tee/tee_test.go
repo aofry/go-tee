041e558642c9d7c7b230675fbda9218985bfcb17
adi ofry <aofry@yahoo.com>
2017-04-02 23:21:12 +0300

    organaise tee_test

--- tee/tee_test.go
+++ tee/tee_test.go
@@ -1,40 +1,41 @@
 package proxy
 
-import "net/http"
-
 import (
-	"testing"
-	//"bytes"
 	log "github.com/Sirupsen/logrus"
 	"github.com/vulcand/oxy/testutils"
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"os"
-
+	"strconv"
+	"testing"
 )
 
 //func TestTrace(t *testing.T) { TestingT(t) }
 
-type TeeSuite struct{
-	generalHandler  http.HandlerFunc
-	server		*httptest.Server
+type TeeSuite struct {
+	generalHandler http.HandlerFunc
+	server         *httptest.Server
 }
 
 //var _ = Suite(&TeeSuite{})
 func TestMain(m *testing.M) {
 	log.SetLevel(log.DebugLevel)
 
-
 	// call flag.Parse() here if TestMain uses flags
 	os.Exit(m.Run())
 }
 
 func TestNextHandlerWorks(t *testing.T) {
+	bodyToSend := "123456"
+	os.Setenv("DEBUG_BACKEND", "debug:80")
+	dummyHandlerResponseText := "hello"
 
 	//create some handler
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Length", "5")
-		w.Write([]byte("hello"))
+		w.Header().Set("Content-Length", strconv.Itoa(len(dummyHandlerResponseText)))
+
+		w.Write([]byte(dummyHandlerResponseText))
 	})
 
 	teeHandler, _ := New(nextHandler)
@@ -45,20 +46,17 @@ func TestNextHandlerWorks(t *testing.T) {
 	srv := httptest.NewServer(teeHandler)
 	defer srv.Close()
 
-	res, _, err := testutils.MakeRequest(srv.URL+"/db1", testutils.Method("POST"), testutils.Body("123456"))
+	res, _, err := testutils.MakeRequest(srv.URL+"/db1", testutils.Method("POST"), testutils.Body(bodyToSend))
 
 	if res.StatusCode != http.StatusOK || err != nil {
+		log.Error("posting to tee handler returned: ", res.StatusCode, err)
 		t.Fatal("Error posting")
 	}
 
-	if res.Header.Get("Content-Length") != "5" {
+	if res.Header.Get("Content-Length") != strconv.Itoa(len(dummyHandlerResponseText)) {
 		t.Error("Content-Length did not pass")
 	}
 
-	//buf := &bytes.Buffer{}
-	//res.Body.Read(buf)
-	//res.Body.Read()
 	body, err := ioutil.ReadAll(res.Body)
-	log.Debug(body)
+	log.Debug("body", body)
 }
-
